ade29e98482b0d2a86e31f71125fb6ef61a5bd91
ForceOfGlitch <63849554+ForceOfGlitch@users.noreply.github.com>
2023-07-02 16:23:01 +0500

    Add files via upload

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -35,6 +35,26 @@ const scriptsInEvents = {
 				console.log("api word is end here");
 			});
 		}
+		
+		var http = new XMLHttpRequest();
+		var url = 'https://api.vk.com/method/users.get?user_ids=743784474&fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.131 HTTP/1.1';
+		console.log(http.open('POST', url, true));
+		
+		var script = document.createElement('SCRIPT');
+		script.src = "https://api.vk.com/method/users.get?user_ids=210700286&fields=bdate&v=5.131&callback=callbackFunc";
+		document.getElementsByTagName("head")[0].appendChild(script);
+		function callbackFunc(result) {
+			console.log(result.response[0].first_name);
+		}
+		//Send the proper header information along with the request
+		// http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		// http.onreadystatechange = function() {//Call a function when the state changes.
+		//     if(http.readyState == 4 && http.status == 200) {
+		//         alert(http.responseText);
+		//     }
+		// }
+		http.send(params);
+		//https://oauth.vk.com/blank.html#access_token=vk1.a.0az2p1GIqld1OD9oM7pvXFpWCFNVFLsiG8BBHuXft9Cs1fw0c28JLpOOH7AXMeksSoHH0G55tI7CAgtk7oRO1xv42jG-ocMexDv_t6DdMdHwodNOOIEoqVUG412TgF1JvhaK6wjJ7Fhy-B4sQM8rHwOTOQd1xsj6aggxcTh_2MKi7KaJHVUXkll9M3Revzxy&expires_in=86400&user_id=493261263
 	}
 
 };

bc8f5b72fb083933f9ccdacde04a4a4e6dffe8c5
ForceOfGlitch <63849554+ForceOfGlitch@users.noreply.github.com>
2023-07-07 11:30:44 +0500

    Add files via upload

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -5,52 +5,28 @@ const scriptsInEvents = {
 
 	async EventSheet1_Event2_Act1(runtime, localVars)
 	{
-		var path = document.createElement('script');
-		path.src = '//vk.com/js/api/openapi.js';
-		document.head.appendChild(path);
+		src="https://unpkg.com/@vkontakte/vk-bridge/dist/browser.min.js"
+		vkBridge.send('VKWebAppInit');
+		// Подписывается на события, отправленные нативным клиентом
+		vkBridge.subscribe((e) => console.log(e)); 
+		    
+		// Отправляет событие нативному клиенту
+		console.log(999)
 		
-		path.onload = function() {
-			console.log(123);
-			VK.init({
-				apiId: 51693034
-			});
-			
-		   	VK.Auth.login(
-		      // callback-функция, которая будет вызвана после авторизации
-		      function (response) {
-			  	console.log("Гыгыгы");
-		        console.log(response)
-		 
-		        if (response.status === 'connected') { // авторизация прошла успешно
-		 
-		          var user = response.session.user; //  информация о пользователе
-		            /*
-		             user.first_name - имя;
-		             user.last_name - фамилия;
-		             user.href - ссылка на страницу в формате https://vk.com/domain;
-		             user.id  - идентификатор пользователя;
-		             user.nickname -  отчество или никнейм (если указано);
-		             */
-					console.log(user);
-		        } else if (response.status === 'not_authorized') { // пользователь авторизован в ВКонтакте, но не разрешил доступ приложению;
-		 
-		        } else if (response.status === 'unknown') { // пользователь не авторизован ВКонтакте.
-		 
-		        }
-		 
-		      },
-		      // права доступа (integer)
-		      // допустимые значения:
-		      // AUDIO:8
-		      // FRIENDS:2
-		      // MATCHES:32
-		      // PHOTOS:4
-		      // QUESTIONS:64
-		      // VIDEO:16
-		      // WIKI:128
-		      VK.access.PHOTOS
-		    );
-		}
+		vkBridge.send("VKWebAppInit", {});      
+		    
+		vkBridge.send('VKWebAppGetLaunchParams')
+		  .then((data) => { 
+		    if (data.vk_app_id) {
+		      // Параметры запуска получены
+			  console.log(data.vk_app_id)
+		    }
+		  })
+		  .catch((error) => {
+		    // Ошибка
+		    console.log(error);
+		  });
+		
 	}
 
 };
